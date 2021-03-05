80b12a2a5fb802aea9603acc89f494586c3f0ccd
Your Name <heung_chun@hotmail.com>
2021-03-06 07:19:49 +1100

    updated queries file

---
+++ Working code/New_quries.sql
@@ -0,0 +1,72 @@
+--Drop any tables if they exist
+DROP TABLE IF EXISTS ato CASCADE;
+DROP TABLE IF EXISTS suburb CASCADE;
+
+-- Create ATO tables for data to be loaded into 
+CREATE TABLE ato (
+	State TEXT,
+	postcode VARCHAR NOT NULL PRIMARY KEY,
+	No_Individuals INT,
+	Total_Taxable_$ NUMERIC,
+	Total_Donation_$ NUMERIC,
+	area TEXT
+);
+
+SELECT * FROM ato;
+
+-- Create Suburb tables for data to be loaded into 
+CREATE TABLE suburb (
+	postcode VARCHAR NOT NULL PRIMARY KEY,
+	suburb VARCHAR,
+	state VARCHAR
+);
+
+SELECT * FROM suburb;
+
+-- join table base on the postcode
+SELECT ato.State, ato.postcode, suburb. suburb, ato.No_Individuals, ato.Total_Taxable_$,
+ato.Total_Donation_$, ato.area
+FROM ato
+INNER JOIN suburb ON ato.postcode = suburb.postcode
+
+-- "Total expected donation index" (total donation / total population)
+SELECT ROUND(SUM(Total_Donation_$) / SUM(No_Individuals)) AS "Total Expected Donation Index"
+FROM ato
+
+-- "Metro expected donation index" (metro area donation / metro population)
+SELECT ROUND(SUM(Total_Donation_$) / SUM(No_Individuals)) AS "Metro Expected Donation Index"
+FROM ato
+WHERE area = 'Metro'
+GROUP BY area;
+
+-- "Regional expected donation index" (Regional area donation / Regional population)
+SELECT ROUND(SUM(Total_Donation_$) / SUM(No_Individuals)) AS "Regional Expected Donation Index"
+FROM ato
+WHERE area = 'Regional'
+GROUP BY area;
+
+-- Show the donation by postcode population
+SELECT ato.postcode, suburb. suburb, ROUND(Total_Donation_$ / No_Individuals) AS "Donation by postcode population"
+FROM ato
+INNER JOIN suburb ON ato.postcode = suburb.postcode
+
+-- create another column, that compares the value in the column above against the 'expected donation index' 
+-- and labels the postcode as "generous" or "no"
+--------add column for donation by area population into ato table 
+ALTER TABLE ato
+
+ADD COLUMN "Donation by postcode population" INTEGER;
+UPDATE ato SET "Donation by postcode population"=ROUND(Total_Donation_$ / No_Individuals);
+
+ALTER TABLE ato
+ALTER COLUMN "Donation by postcode population" SET NOT NULL;
+
+SELECT * FROM ato
+
+--------Compare Donation by postcode population with Total expected donation index (282)
+SELECT ato.postcode, suburb. suburb,
+       CASE WHEN "Donation by postcode population">282 THEN 'generous'
+            WHEN "Donation by postcode population"<282 THEN 'no'
+            ELSE 'other'
+       END
+    FROM ato INNER JOIN suburb ON ato.postcode = suburb.postcode;
