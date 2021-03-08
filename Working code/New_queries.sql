49093444a878513bf93f6b5f2045e937c881cd5d
Franz365 <74512752+Franz365@users.noreply.github.com>
2021-03-08 14:48:10 +1100

    Updated schema file, queries and cleanup of the notebooks

--- Working code/New_queries.sql
+++ Working code/New_queries.sql
@@ -1,28 +1,3 @@
---Drop any tables if they exist
-DROP TABLE IF EXISTS ato CASCADE;
-DROP TABLE IF EXISTS suburb CASCADE;
-
--- Create ATO tables for data to be loaded into 
-CREATE TABLE ato (
-	State TEXT,
-	postcode VARCHAR NOT NULL PRIMARY KEY,
-	No_Individuals INT,
-	Total_Taxable_$ NUMERIC,
-	Total_Donation_$ NUMERIC,
-	area TEXT
-);
-
-SELECT * FROM ato;
-
--- Create Suburb tables for data to be loaded into 
-CREATE TABLE suburb (
-	postcode VARCHAR NOT NULL PRIMARY KEY,
-	suburb VARCHAR,
-	state VARCHAR
-);
-
-SELECT * FROM suburb;
-
 -- join table base on the postcode
 SELECT ato.State, ato.postcode, suburb. suburb, ato.No_Individuals, ato.Total_Taxable_$,
 ato.Total_Donation_$, ato.area
--- Working code/schema.sql
+++ Working code/schema.sql
@@ -1,26 +1,26 @@
 --Drop any tables if they exist
 DROP TABLE IF EXISTS ato CASCADE;
-DROP TABLE IF EXISTS metro CASCADE;
 DROP TABLE IF EXISTS suburb CASCADE;
 
--- Create tables for data to be loaded into
+-- Create ATO tables for data to be loaded into 
 CREATE TABLE ato (
 	State TEXT,
 	postcode VARCHAR NOT NULL PRIMARY KEY,
 	No_Individuals INT,
 	Total_Taxable_$ NUMERIC,
-	Total_Donation_$ NUMERIC
+	Total_Donation_$ NUMERIC,
+	area TEXT
 );
 
-CREATE TABLE metro (
-	postcode VARCHAR,
-	area TEXT,
-	FOREIGN KEY (postcode) REFERENCES ato (postcode)
-);
+SELECT * FROM ato;
 
+-- Create Suburb tables for data to be loaded into 
 CREATE TABLE suburb (
-	postcode VARCHAR,
+	postcode VARCHAR NOT NULL PRIMARY KEY,
 	suburb VARCHAR,
-	state VARCHAR,
-	FOREIGN KEY (postcode) REFERENCES ato (postcode)	
+	state VARCHAR
 );
+
+SELECT * FROM suburb;
+
+-- Import csv files from the outputfolder into the tables
